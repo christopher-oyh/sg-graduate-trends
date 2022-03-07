edd716ce7fcdd83f948f1c27f74dbfb5ab60daa4
christopher-oyh <71048176+christopher-oyh@users.noreply.github.com>
2022-03-07 17:41:13 +0800

    Commit visual changes to tableau dashboard

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import React from 'react';
 import './App.css';
 
 import BarChart from './Components/BarChart'
+import Body from './Components/Body';
 import Dashboard from './Components/Dashboard';
+import Navbar from './Components/Navbar';
+import Title from './Components/Title';
 
 function App() {
 
   return(
     <div>
-      <p>Hello </p>
-      <BarChart/>
+      
+      <Navbar/>
+      {/* <Title/> */}
+      {/* <BarChart/> */}
+      {/* <Body/> */}
       <Dashboard/>
     </div>
   )
--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -6,7 +6,7 @@ const Dashboard = () => {
 
     const ref = useRef(null)
     //Tableau URL
-    const url = "https://public.tableau.com/shared/WMWJFZBSJ?:display_count=n&:origin=viz_share_link" 
+    const url = "https://public.tableau.com/shared/BXN7YZ4JF?:display_count=n&:origin=viz_share_link" 
     // const url = "https://public.tableau.com/shared/WMWJFZBSJ" 
 
     const initViz = () => {
@@ -17,10 +17,18 @@ const Dashboard = () => {
         initViz();
     }, [])
 
+    function vizResize() {
+        var width = document.getElementById("resizeWidth").value;
+        var height = document.getElementById("resizeHeight").value;
+
+        tableau.Viz.setFrameSize(parseInt(width, 10), parseInt(height, 10));
+    }
+
     return (
         <div>
             <h1>Tableau Dashboard</h1>
-            <div ref={ref} > </div>
+            <div ref={ref}  > </div>
+            
         </div>
     )
 }
